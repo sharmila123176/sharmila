aedbbc722abdf09354b9174ad51771257e6bb41c
sharmila <pulamarasetti123@gmail.com>
2024-12-05 14:04:58 +0530

    committing number,two

---
+++ two.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Calculate the sum
+sum = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is {sum}.")
